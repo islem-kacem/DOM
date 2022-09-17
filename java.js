bc7f39a193e8d94ce34cd9f139188fe74243df4d
islem kacem <islemkacem2@gmail.com>
2022-09-17 12:56:51 +0100

    java

---
+++ java.js
@@ -0,0 +1,15 @@
+var counterVal = 0;
+
+function incrementClick() {
+    updateDisplay(++counterVal);
+}
+
+function resetCounter() {
+    counterVal = 0;
+    updateDisplay(counterVal);
+}
+
+function updateDisplay(val) {
+    document.getElementById("counter-label").innerHTML = val;
+}
+
